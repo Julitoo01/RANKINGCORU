3418cacbd4e0cc6077a68d8edd53a47db2ce1cae
Julio Eugenio Lapuente <jlapujun@gmail.com>
2026-07-06 12:04:55 +0000

    remove backend url check

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,27 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
+
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { StoreProvider } from "./hooks/useGlobalReducer";
-import { BackendURL } from "./components/BackendURL";
 
-const Main = () => {
-  if (!import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_URL === "") {
-    return (
-      <React.StrictMode>
-        <BackendURL />
-      </React.StrictMode>
-    );
-  }
-
-  return (
-    <React.StrictMode>
-      <StoreProvider>
-        <RouterProvider router={router} />
-      </StoreProvider>
-    </React.StrictMode>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
+  </React.StrictMode>
+);
