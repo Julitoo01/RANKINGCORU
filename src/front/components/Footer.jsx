e95e5c4697e5428df97c59eff8ba4f949444c6ca
Julio Eugenio Lapuente <jlapujun@gmail.com>
2026-07-27 09:01:55 +0000

    Make footer more discreet

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -4,10 +4,7 @@ export const Footer = () => {
   return (
     <footer className="app-footer">
       <div className="app-footer-content">
-        <div>
-          <strong>Fuera de Pista</strong>
-          <span>Ranking social de pádel en A Coruña</span>
-        </div>
+        <p>© 2026 Fuera de Pista · Ranking social de pádel</p>
 
         <nav className="app-footer-links">
           <Link to="/terms">Términos</Link>
